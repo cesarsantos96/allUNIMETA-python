58d14543d3246105b0ccd6e5426169e6fd4335b2
Cesar Santos <59935896+cesarsantos96@users.noreply.github.com>
2022-10-09 15:30:36 -0500

    Delete 08.py

--- 08.py
+++
@@ -1,8 +0,0 @@
-valor_hora = float(input("Quanto que você recebe por hora?"))
-
-horas_trabalhada = float(input("Quantas horas você trabalhou"))
-
-salario = valor_hora * horas_trabalhada
-
-print(
-    f" Você trabalhou {horas_trabalhada} horas, então o seu salário é de R$ {salario:}")
