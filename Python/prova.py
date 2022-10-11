afb41b7faf1856a97b9bebda12e0137197b69609
Cesar Santos <csar.santos18@gmail.com>
2022-10-10 19:55:04 -0500

    resolicao-prova

---
+++ Python/prova.py
@@ -0,0 +1,11 @@
+def calcularConsumoMedio(distanciaTotal, TotalCombustivel):
+    ConsumeMedio = distanciaTotal/TotalCombustivel
+    print(f'{ConsumeMedio}Km/l')
+
+    calcularConsumoMedio(10, 10)
+
+    calcularConsumoMedio(20, 20)
+
+    calcularConsumoMedio(54, 10)
+
+    calcularConsumoMedio(10, 10)
